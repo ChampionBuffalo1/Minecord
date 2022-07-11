fb4bf36bb789ca2e7173744c85f2e6ac43b7c1eb
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-07-11 23:24:59 +0530

    Removing colorcodes from playerName
    Removes `ChatColor` codes from `player.getDisplayName()` before
    posting the message to discord

--- src/main/java/com/champ/minecord/listeners/ChatEventListener.java
+++ src/main/java/com/champ/minecord/listeners/ChatEventListener.java
@@ -3,6 +3,7 @@ package com.champ.minecord.listeners;
 import com.champ.minecord.Minecord;
 import com.champ.minecord.discord.DiscordJDAConnection;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,7 +18,7 @@ public class ChatEventListener implements Listener {
     public void onChatMessage(AsyncPlayerChatEvent event) {
         Player plyr = event.getPlayer();
         String message = event.getMessage();
-        StringBuilder builder = new StringBuilder(plyr.getDisplayName())
+        StringBuilder builder = new StringBuilder(ChatColor.stripColor(plyr.getDisplayName()))
                 .append(": ")
                 .append(message);
         // Just for a test run
--- src/main/java/com/champ/minecord/listeners/JoinLeaveListener.java
+++ src/main/java/com/champ/minecord/listeners/JoinLeaveListener.java
@@ -5,6 +5,7 @@ import com.champ.minecord.discord.DiscordJDAConnection;
 import com.champ.minecord.utility.ConfigDefaults;
 import net.dv8tion.jda.api.entities.TextChannel;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,7 +20,8 @@ public class JoinLeaveListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         TextChannel channel = DiscordJDAConnection.getTextChannel();
-        String playerName = event.getPlayer().getDisplayName();
+        String playerName = ChatColor.stripColor(event.getPlayer().getDisplayName());
+
         String emoji = Minecord.getPlugin().getConfig().getString("emojis.join");
         if (emoji == null || emoji.length() == 0)
             emoji = ConfigDefaults.JOIN_EMOJI.getDefault();
@@ -35,7 +37,7 @@ public class JoinLeaveListener implements Listener {
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event) {
         TextChannel channel = DiscordJDAConnection.getTextChannel();
-        String playerName = event.getPlayer().getDisplayName();
+        String playerName = ChatColor.stripColor(event.getPlayer().getDisplayName());
 
         String emoji = Minecord.getPlugin().getConfig().getString("emojis.leave");
         if (emoji == null || emoji.length() == 0)
