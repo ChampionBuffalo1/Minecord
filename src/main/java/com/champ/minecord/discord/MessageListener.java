3a64151965ee0d10409b8bd6dcd797c6e829ef63
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-07-12 21:10:12 +0530

    Boundary check for messageBroadcast

--- src/main/java/com/champ/minecord/discord/MessageListener.java
+++ src/main/java/com/champ/minecord/discord/MessageListener.java
@@ -1,6 +1,7 @@
 package com.champ.minecord.discord;
 
 import com.champ.minecord.Minecord;
+import com.champ.minecord.utility.DiscordChatUtils;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -8,18 +9,26 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class MessageListener extends ListenerAdapter {
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (!event.isFromType(ChannelType.TEXT) || event.getAuthor().isBot() ||
                 !event.getChannel().getId()
-                .equals(Minecord.getPlugin()
-                .getConfig().getString("channelId"))
-            )
+                        .equals(Minecord.getPlugin()
+                                .getConfig().getString("channelId"))
+        )
             return;
         String authorTag = event.getAuthor().getAsTag();
-        String message = event.getMessage().getContentRaw();
-        StringBuilder builder = new StringBuilder(ChatColor.BLUE + authorTag)
+        String message = DiscordChatUtils.cleanMessage(event.getMessage().getContentRaw());
+        StringBuilder builder = new StringBuilder(ChatColor.BLUE + "[#")
+                .append(event.getChannel().getName())
+                .append("] ")
+                .append(authorTag)
                 .append(": " + ChatColor.WHITE).append(message);
-        Bukkit.getServer().broadcastMessage(builder.toString());
+        String str = builder.toString();
+        if (str.length() >= 256) // 256 is the server chat's character limit
+            str = str.substring(0, 256);
+
+        Bukkit.getServer().broadcastMessage(str);
     }
 }
---
+++ src/main/java/com/champ/minecord/utility/DiscordChatUtils.java
@@ -0,0 +1,87 @@
+package com.champ.minecord.utility;
+
+import com.champ.minecord.discord.DiscordJDAConnection;
+import net.dv8tion.jda.api.entities.Channel;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DiscordChatUtils {
+    private final static Pattern emotePattern = Pattern.compile("<(:\\w+:)(\\d{15,22})>");
+    private final static Pattern mentionPattern = Pattern.compile("<@!?(\\d{15,22})>");
+    private final static Pattern roleMentionPattern = Pattern.compile("<@&(\\d{15,22})>");
+    private final static Pattern channelMention = Pattern.compile("<#(\\d{15,22})>");
+    /**
+     * Removes discord specific things from message
+     *
+     * @param discordMessage Input String to clean
+     * @return Properly "cleaned" message string
+     */
+    public static String cleanMessage(String discordMessage) {
+        String withoutEmotes = removeEmotes(discordMessage);
+        return removeMentions(withoutEmotes);
+    }
+
+    /**
+     * Removes irrelevant discord emote ids with just their name i.e, in format :{emote_name}:
+     *
+     * @param input Input string
+     * @return Properly "cleaned" message string
+     */
+    public static String removeEmotes(String input) {
+        Matcher match = emotePattern.matcher(input);
+        if (!match.find()) return input;
+        return match.replaceAll("$1");
+    }
+
+    /**
+     * @param input
+     * @return Properly "cleaned" message string
+     */
+    public static String removeMentions(String input) {
+        String withoutChannelMention = removeChannel(input);
+        String withoutRoleMention = removeRole(withoutChannelMention);
+        Matcher match = mentionPattern.matcher(withoutRoleMention);
+        if (!match.find()) return withoutRoleMention;
+        String id = match.group(1);
+        String toReplaceWith = "@invalid-user";
+        Member member = DiscordJDAConnection.getGuild().getMemberById(id);
+	if (member != null)
+	  toReplaceWith = "@" + member.getUser().getName();
+        return match.replaceAll(toReplaceWith);
+    }
+
+    public static String removeRole(String input) {
+        Matcher match = roleMentionPattern.matcher(input);
+        if (!match.find()) return input;
+        String id = match.group(1);
+        String toReplaceWith = "deleted-role";
+        Role role = DiscordJDAConnection.getGuild().getRoleById(id);
+        if (role != null)
+            toReplaceWith = "@" + role.getName();
+        return match.replaceAll(toReplaceWith);
+    }
+
+    public static String removeChannel(String input) {
+        Matcher match = channelMention.matcher(input);
+        if (!match.find()) return input;
+        String id = match.group(1);
+        String toReplaceWith = "#deleted-channel";
+        Channel channel = DiscordJDAConnection.getGuild().getChannelById(Channel.class, id);
+        if (channel != null)
+            toReplaceWith = "#" + channel.getName();
+        return match.replaceAll(toReplaceWith);
+    }
+
+    /**
+     * StringBuilder overload for cleanMessage method
+     *
+     * @param messageBuilder
+     * @return Properly "cleaned" message string
+     */
+    public static String cleanMessage(StringBuilder messageBuilder) {
+        return cleanMessage(messageBuilder.toString());
+    }
+}
