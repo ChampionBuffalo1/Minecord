132e3cfc0287caf60f8070114fbeea4a9a18a4bd
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-07-14 23:12:00 +0530

    Fixed support for tags (Not fully fixed yet)

--- src/main/java/com/champ/minecord/utility/MinecraftChatUtils.java
+++ src/main/java/com/champ/minecord/utility/MinecraftChatUtils.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class MinecraftChatUtils {
     private final static Pattern simpleEmote = Pattern.compile(":(\\w+):");
     private final static Pattern simpleChannel = Pattern.compile("#([a-z\\d-]+)");
-    private final static Pattern simpleMember = Pattern.compile("@([\\w\\d]+(#\\d{4})?)");
+    private final static Pattern simpleMember = Pattern.compile("@([\\w\\d-]+(#\\d{4})?)");
 
     public static String inject(StringBuilder builder) {
         return inject(builder.toString());
@@ -39,7 +39,7 @@ public class MinecraftChatUtils {
         if (!match.find()) return inputString;
         return match.replaceAll(matchResult -> {
             String name = matchResult.group(1);
-            List<TextChannel> channels = DiscordJDAConnection.getJda().getTextChannelsByName(name, true);
+            List<TextChannel> channels = DiscordJDAConnection.getGuild().getTextChannelsByName(name, true);
             if (channels.isEmpty()) return matchResult.group();
             return channels.get(0).getAsMention();
         });
@@ -52,31 +52,34 @@ public class MinecraftChatUtils {
         // Doesn't work as intended rn
         return match.replaceAll(matchResult -> {
             String name = matchResult.group(1).toLowerCase();
+            boolean isWithTag = matchResult.group(2) != null;
             List<Member> members = cacheView
                     .parallelStream()
                     // Removing all members whose name's length is less than what was provided for search
                     // Removing anyone whose name doesn't startsWith the name we are looking for
-                    .filter(member ->
-			   // TODO: Use getAsTag if matchResult.group(2) is present
-                            member.getUser().getName().length() >= name.length() &&
-                                    member.getUser().getName().toLowerCase().startsWith(name)
-                    )
+                    .filter(member -> (isWithTag ?
+                            member.getUser().getAsTag().length() >= name.length() :
+                            member.getUser().getName().length() >= name.length()
+                    ) && member.getUser().getName().toLowerCase().startsWith(name))
                     .toList();
-            // Closes the stream even if it wasn't empty
             if (members.isEmpty())
                 return matchResult.group();
-            if (members.size() == 1)
+            if (members.size() == 1) {
+                PluginLogger.info(members.get(0).getAsMention());
                 return members.get(0).getAsMention();
+            }
             // Priority to finding a user with exact match
             // If no exact match is found then we try to find a user with username Starting with the query(?)
             for (Member m : members) {
-                if (m.getUser().getAsTag().equalsIgnoreCase(name) || m.getUser().getName().equalsIgnoreCase(name))
+                if (m.getUser().getAsTag().equalsIgnoreCase(name) ||
+                        m.getUser().getName().equalsIgnoreCase(name))
                     return m.getAsMention();
             }
             for (Member m : members) {
                 if (m.getUser().getName().toLowerCase().startsWith(name))
                     return m.getAsMention();
             }
+            PluginLogger.severe("Unexpected error in trying to find user, identify dev");
             // Since we are filtering users and returning if no user could be determined we should, In theory never actually return null
             // Matcher#replaceAll() will throw an error if we ever actually return null
             return null;
