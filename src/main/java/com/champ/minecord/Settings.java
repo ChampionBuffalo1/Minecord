5a967bedb8155c395ec1681817c443d432c9b828
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-07-18 23:28:25 +0530

    Properly using Optionals and fixed the invalid format string for player avatar URL

--- src/main/java/com/champ/minecord/Settings.java
+++ src/main/java/com/champ/minecord/Settings.java
@@ -2,6 +2,7 @@ package com.champ.minecord;
 
 import com.champ.minecord.utility.ConfigDefaults;
 import com.champ.minecord.utility.PluginLogger;
+import net.dv8tion.jda.api.entities.Webhook;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.Optional;
@@ -11,32 +12,32 @@ public class Settings {
 
     public static Optional<String> getBotToken() {
         String token = config.getString("token");
-        if (token == null || token.length() == 0 || token.equalsIgnoreCase(ConfigDefaults.TOKEN.getDefault())) {
+        if (commonErrors(token) || token.equalsIgnoreCase(ConfigDefaults.TOKEN.getDefault())) {
             PluginLogger.unrecoverable("Bot Token not found in config.yml, disabling plugin");
-            return Optional.ofNullable(null);
+            return Optional.empty();
         }
-        return Optional.ofNullable(token);
+        return Optional.of(token);
     }
 
     public static Optional<String> getChannelId() {
         String id = config.getString("channelId");
-        if (id == null || id.length() == 0 || id.equalsIgnoreCase(ConfigDefaults.CHANNEL_ID.getDefault()))
-            return Optional.ofNullable(null);
-        return Optional.ofNullable(id);
+        if (commonErrors(id) || id.equalsIgnoreCase(ConfigDefaults.CHANNEL_ID.getDefault()))
+            return Optional.empty();
+        return Optional.of(id);
     }
 
     public static Optional<String> getGuildId() {
         String id = config.getString("guildId");
-        if (id == null || id.length() == 0 || id.equalsIgnoreCase(ConfigDefaults.GUILD_ID.getDefault()))
-            return Optional.ofNullable(null);
-        return Optional.ofNullable(id);
+        if (commonErrors(id) || id.equalsIgnoreCase(ConfigDefaults.GUILD_ID.getDefault()))
+            return Optional.empty();
+        return Optional.of(id);
     }
 
     public static Optional<String> getEmote(String path) {
         String emote = config.getString(path);
-        if (emote == null || emote.length() == 0 || !emote.matches("<:\\w+:\\d{15,22}>"))
-            return Optional.ofNullable(null);
-        return Optional.ofNullable(emote);
+        if (commonErrors(emote) || !emote.matches("<:\\w+:\\d{15,22}>"))
+            return Optional.empty();
+        return Optional.of(emote);
     }
 
     public static boolean getShowIp() {
@@ -48,10 +49,16 @@ public class Settings {
     }
 
     public static Optional<String> getWebhookURL() {
-        return Optional.ofNullable(config.getString("webhookURL"));
+        String webhook = config.getString("webhookURL");
+        if (commonErrors(webhook) || !Webhook.WEBHOOK_URL.matcher(webhook).matches())
+            return Optional.empty();
+        return Optional.of(webhook);
     }
 
     public static String getPlayerAvatarURL(String uuid) {
-        return String.format("https://crafthead.net/helm/{uuid}", uuid);
+        return String.format("https://crafthead.net/helm/%s", uuid);
+    }
+    private static boolean commonErrors(String toCheck) {
+        return toCheck == null || toCheck.length() == 0;
     }
 }
